230ecea371b7722bfe71b4334da254c88e687440
Rizky Pratama <rizky.pratama98988@gmail.com>
2021-07-20 19:05:09 +0700

    add model and migration

---
+++ database/migrations/2021_07_19_190941_create_essays_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEssaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('essays', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('title')->nullable();
+            $table->string('lecturer')->nullable();
+            $table->string('year')->nullable();
+            $table->string('abstract_id')->nullable();
+            $table->string('abstract_en')->nullable();
+            $table->string('keywords')->nullable();
+            $table->string('file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('essays');
+    }
+}
